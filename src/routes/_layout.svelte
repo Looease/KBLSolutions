b6f203540832b9f368576f9c7ac74d0be60e0f3a
Gareth Clubb <gareth.clubb@zoopla.co.uk>
2020-07-01 15:07:56 +0100

    Fixing slug typo bug

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,9 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 	import Footer from '../components/Footer.svelte';
-	export let segment;
 </script>
 
-
-<Nav {segment}/>
+<Nav />
 
 <main>
 	<slot></slot>
--- src/routes/products/[slug].json.js
+++ src/routes/products/[slug].json.js
@@ -33,4 +33,3 @@ export function get(req, res, next) {
 		}));
 	}
 }
- [slug].svelte
